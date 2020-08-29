9ad882e2e5e9bf8d72d38cb0aa1da4cff2cd688f
Ana Karine <anakarine.ti@gmail.com>
2020-08-29 04:12:55 -0300

    delete bloco_1

--- bloco_1/dia_5/exercicio1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Shell Script que imprime na tela a frase: "Shell Script é demais!"
-
-echo "Shell Script é demais!"
--- bloco_1/dia_5/exercicio2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# A variável FRASE recebe o conteúdo da mensagem: "Shell Script com variáveis é demais!"
-
-FRASE="Shell Script com variáveis é demais!"    
-  echo $FRASE 
