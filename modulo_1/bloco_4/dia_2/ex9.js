4b409ddf973afa495fd5bfee09f969c5fdccc769
Ana Karine <anakarine.ti@gmail.com>
2020-08-29 19:54:04 -0300

    divisao dos elementos array por 2

---
+++ modulo_1/bloco_4/dia_2/ex9.js
@@ -0,0 +1,11 @@
+let numbers = [];
+
+for (let i = 1; i < 26; i += 1) {
+  numbers.push(i);
+}
+
+// imprime o resultado da divisão de cada um dos elementos 
+// do array numbers por 2
+for (let i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i] / 2);
+  };
