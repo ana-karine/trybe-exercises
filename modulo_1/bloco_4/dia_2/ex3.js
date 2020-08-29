e4bde9c67eb8b640136316e7a9579151c8c8a375
Ana Karine <anakarine.ti@gmail.com>
2020-08-29 19:26:18 -0300

    media aritmetica array

---
+++ modulo_1/bloco_4/dia_2/ex3.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+  sum += numbers[i];
+}
+
+// Calcula a média aritmética dos valores contidos no array
+let average = sum / numbers.length;
+
+console.log(average);
