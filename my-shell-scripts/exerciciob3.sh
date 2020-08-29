6ce4e6627ef64d863a7caba8b42f92fb39bbd42f
Ana Karine <anakarine.ti@gmail.com>
2020-08-29 03:39:05 -0300

    my-shell-scripts

--- my-shell-scripts/exerciciob3.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
- #Shell Script que renomeia todos os arquivos do diretório atual terminados em ".png", 
- #de forma que o nome do arquivo comece com a data atual no formato "YYYY-MM-DD"
-  DAY=$(date +%F)
-  FILES='ls *.png'
-  for FILE in $FILES
-   do
-      mv $FILE ${DAY}-${FILE}
-   done
