18857e635e900a11ac2393cf25b3ec088610887c
Ana Karine <anakarine.ti@gmail.com>
2020-08-29 12:13:31 -0300

    shell-script-change

--- modulo_1/bloco_1/dia_5/exercicio1.sh
+++ modulo_1/bloco_1/dia_5/exercicio1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Shell Script que imprime na tela a frase: "Shell Script é demais!"
+# Eu sou Tryber e...
 
+# Shell Script que imprime na tela a frase: "Shell Script é demais!"
 echo "Shell Script é demais!"
