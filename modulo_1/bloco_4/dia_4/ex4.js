caaecb13ffc64be1a92372eb036a3dcb40161960
Ana Karine <anakarine.ti@gmail.com>
2020-09-02 11:47:12 -0300

    mostra os valores das chaves dos objetos

---
+++ modulo_1/bloco_4/dia_4/ex4.js
@@ -0,0 +1,11 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
+};
+
+// mostra os valores de todas as chaves do objeto
+for(let chaves in info){
+    console.log(info[chaves]);
+}
