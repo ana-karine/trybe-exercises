18c79c43cb9cab00c1c2f61e43e1149d15e592c9
Ana Karine <anakarine.ti@gmail.com>
2020-09-02 11:42:06 -0300

    mostrar todas as chaves do objeto

---
+++ modulo_1/bloco_4/dia_4/ex3.js
@@ -0,0 +1,13 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
+};
+
+// mostre todas as chaves do objeto
+for(let chaves in info){
+    console.log(chaves);
+}
+
+  
