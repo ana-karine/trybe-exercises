54f74a697bfd0f5817a30e9ad1f96bca3a0e2836
Ana Karine <anakarine.ti@gmail.com>
2020-09-02 11:35:03 -0300

    inserir nova propriedade no objeto

---
+++ modulo_1/bloco_4/dia_4/ex2.js
@@ -0,0 +1,19 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+// insire no objeto "info" uma nova propriedade com o nome de 
+// chave "recorrente" e o valor "Sim"
+info.recorrente = 'Sim';
+
+console.log(info);
+
+
+// outra forma de resolver o exercício
+/*
+info['recorrente'] = 'Sim';
+
+console.log(info);
+*/
