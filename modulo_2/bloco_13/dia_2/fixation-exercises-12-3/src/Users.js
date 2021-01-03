7439714e4a13910e3e26d17cafccf0d2b815e600
Ana Karine <anakarine.ti@gmail.com>
2021-01-03 18:07:18 -0300

    exercises 13-2

---
+++ modulo_2/bloco_13/dia_2/fixation-exercises-12-3/src/Users.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Users = ({ greetingMessage = 'Hi There', match }) => (
+  <div>
+    <h2> Users </h2>
+    <p> {greetingMessage}, this is my awesome Users component </p>
+    <p>ID: {match.params.id}</p>
+  </div>
+);
+
+export default Users;
+
+
+/*
+import React from 'react';
+
+const Users = ({ greetingMessage = 'Hi There' }) => (
+  <div>
+    <h2> Users </h2>
+    <p> {greetingMessage}, this is my awesome Users component </p>
+  </div>
+);
+
+export default Users;
+*/
